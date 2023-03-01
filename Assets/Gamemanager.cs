6e338a7ea87c5293e5d2100fc054d73cab690247
ThomasJaffe <92303180+ThomasJaffe@users.noreply.github.com>
2023-03-01 13:04:00 +0100

    change menu opening

--- Assets/Gamemanager.cs
+++ Assets/Gamemanager.cs
@@ -13,7 +13,7 @@ public class Gamemanager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
+        if (Input.GetButtonDown("Cancel")) {
             isMenuOpen = !isMenuOpen;
         }
         if (isMenuOpen) {
